ccac3623a8dfd50d1e4b3f6faf1ea1f9e30e80e4
zmq <zmq@qq.com>
2020-02-19 01:19:30 +0800

    zmq

--- check_by_face/check_by_face_controller/src/main/webapp/js/changePwd.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $("#exit").click(function(){
-        alert("退出成功");
-//  window.location.replace("http://www.baidu.com");
-    });
-});
--- check_by_face/check_by_face_service/src/main/java/com/pyp/check_by_face/services/ICourseService.java
+++
@@ -1,4 +0,0 @@
-package com.pyp.check_by_face.services;
-
-public interface ICourseService {
-}
