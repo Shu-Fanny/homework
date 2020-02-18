33b5a88bfe0707d87a38130ca6e601f7e12c4c73
zmq <zmq@qq.com>
2020-02-19 00:45:42 +0800

    first

--- store/store_service/src/main/java/com/pyp/cast/store/service/IAdminService.java
+++
@@ -1,7 +0,0 @@
-package com.pyp.cast.store.service;
-
-import com.pyp.cast.store.domain.PO.Admin;
-
-public interface IAdminService {
-    Admin findAdminByUserName(String adname)throws Exception;
-}
--- store/store_service/src/main/java/com/pyp/cast/store/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.pyp.cast.store.service;
-
-import com.pyp.cast.store.domain.PO.User;
-
-public interface IUserService {
-    User findUserByUserName(String username)throws Exception;
-
-    void addUser(User user)throws Exception;
-}
